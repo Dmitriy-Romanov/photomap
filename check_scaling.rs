d0b72122e1c847a63b4e131baa2fe240f1eac104
Dmitrii Romanov <spiteman@gmail.com>
2025-11-20 16:50:18 +0100

    chore: Remove `check_scaling` utility and refactor CI/CD workflows for a workspace-isolated `libheif` installation.

--- check_scaling.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let factors = turbojpeg::Decompressor::supported_scaling_factors();
-    println!("Supported factors:");
-    for f in factors {
-        println!("{}/{}", f.num(), f.denom());
-    }
-}
